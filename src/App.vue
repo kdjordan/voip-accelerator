0a73a0768c42e151feda144ecec22fa6587daace
kdjordan <k.dean.jordan@gmail.com>
2024-06-25 18:53:30 -0700

    holy shit it works

--- src/App.vue
+++ src/App.vue
@@ -1,246 +1,246 @@
 <template>
-	<div>
-		<TheHeader />
-		<div class="mx-auto p-6 space-y-6 pt-32">
-			<div
-				v-if="!isReporting"
-				class="max-w-[1200px] mx-auto flex flex-col gap-4"
-			>
-				<UploadComponent
-					mssg="Upload YOUR rates as CSV. <br /><br /> You can drag and drop or click <span style='color:blue;'>here</span> to select from your computer."
-					v-if="file1 === null"
-					@file-selected="handleFile"
-				/>
-				<CompleteUpload
-					v-if="file1 !== null"
-					:mssg="`Your rates from ${details?.fileName1} have been accepted`"
-				/>
-				<UploadComponent
-					mssg="Upload your CLIENT rates as CSV. <br /><br /> You can drag and drop or click <span style='color:blue;'>here</span> to select from your computer."
-					v-if="file2 === null"
-					@file-selected="handleFile"
-				/>
-				<CompleteUpload
-					v-if="file2 !== null"
-					:mssg="`The carrier rates from ${details?.fileName2} have been accepted`"
-				/>
-				<button
-					v-if="filesReady"
-					@click="compareFiles"
-					class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition text-center"
-				>
-					Compare Files
-				</button>
-			</div>
-			<div v-else>
-				<GenerateReport
-					v-if="report"
-					:report="report"
-					:details="details"
-				/>
-			</div>
-		</div>
-	</div>
+  <div>
+    <TheHeader />
+    <div class="mx-auto p-6 space-y-6 pt-32">
+      <div
+        v-if="!isReporting"
+        class="max-w-[1200px] mx-auto flex flex-col gap-4"
+      >
+        <UploadComponent
+          mssg="Upload YOUR rates as CSV. <br /><br /> You can drag and drop or click <span style='color:blue;'>here</span> to select from your computer."
+          v-if="file1 === null"
+          @file-selected="handleFile"
+        />
+        <CompleteUpload
+          v-if="file1 !== null"
+          :mssg="`Your rates from ${details?.fileName1} have been accepted`"
+        />
+        <UploadComponent
+          mssg="Upload your CLIENT rates as CSV. <br /><br /> You can drag and drop or click <span style='color:blue;'>here</span> to select from your computer."
+          v-if="file2 === null"
+          @file-selected="handleFile"
+        />
+        <CompleteUpload
+          v-if="file2 !== null"
+          :mssg="`The carrier rates from ${details?.fileName2} have been accepted`"
+        />
+        <button
+          @click="dumpDB"
+          class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition text-center"
+        >
+          DUMP
+        </button>
+        <button
+          v-if="filesReady"
+          @click="compareFiles"
+          class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition text-center"
+        >
+          Compare Files
+        </button>
+      </div>
+      <div v-else>
+        <GenerateReport
+          v-if="report"
+          :report="report"
+          :details="details"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-	import { ref, computed } from 'vue';
-	import UploadComponent from './components/UploadComponent.vue';
-	import CompleteUpload from './components/CompleteUpload.vue';
-	import GenerateReport from './components/GenerateReport.vue';
-	import TheHeader from './components/TheHeader.vue';
-	import { ComparisonReport, StandardizedData } from './types'; // Adjust this based on your actual types
+import { ref, computed } from 'vue';
+import UploadComponent from './components/UploadComponent.vue';
+import CompleteUpload from './components/CompleteUpload.vue';
+import GenerateReport from './components/GenerateReport.vue';
+import TheHeader from './components/TheHeader.vue';
+import { ComparisonReport, StandardizedData } from './types';
 
-	const file1 = ref<File | null>(null);
-	const file2 = ref<File | null>(null);
+const file1 = ref<File | null>(null);
+const file2 = ref<File | null>(null);
 
-	const isReporting = ref<boolean>(false);
-	const report = ref<ComparisonReport | null>(null);
-	const details = ref<{
-		fileName1: string;
-		fileName2: string;
-	} | null>(null);
+const DBversion = ref<number>(1);
 
-	const filesReady = computed(
-		() => file1.value !== null && file2.value !== null
-	);
+const isReporting = ref<boolean>(false);
+const report = ref<ComparisonReport | null>(null);
+const details = ref<{
+  fileName1: string;
+  fileName2: string;
+} | null>(null);
 
-	function handleFile(fileData: {
-		file: File;
-		data: StandardizedData[];
-	}) {
-		// console.log('got file', fileData);
-		if (!details.value || !details.value.fileName1) {
-			details.value = {
-				fileName1: fileData.file.name.split('.')[0],
-				fileName2: ''
-			};
-			storeInIndexedDB(
-				fileData.data,
-				details.value.fileName1
-			);
-		} else if (!details.value.fileName2) {
-			details.value = {
-				fileName1: details.value.fileName1,
-				fileName2: fileData.file.name.split('.')[0],
-			};
-      // console.log('going in second with ', fileData.data, details.value.fileName2);
-      storeInIndexedDB(
-				fileData.data,
-				details.value.fileName2
-			);
-		}
-		
-	}
+const filesReady = computed(
+  () => file1.value !== null && file2.value !== null
+);
 
-	async function compareFiles() {
-		isReporting.value = true;
-		if (!filesReady.value) {
-			alert('Please select both files');
-			return;
-		}
+function dumpDB() {
+  const deleteRequest = indexedDB.deleteDatabase('CSVDatabase');
 
-		const worker = new Worker(
-			new URL('./compareWorker.ts', import.meta.url),
-			{ type: 'module' }
-		);
+  deleteRequest.onsuccess = function () {
+    console.log(`Deleted database 'CSVDatabase' successfully.`);
+  };
 
-		const map1Data = await loadFromIndexedDB(
-			file1.value!.name.split('.')[0]
-		);
-		const map2Data = await loadFromIndexedDB(
-			file2.value!.name.split('.')[0]
-		);
+  deleteRequest.onerror = function (event) {
+    console.error(`Error deleting database 'CSVDatabase':`, deleteRequest.error);
+  };
 
-		worker.onmessage = function (event) {
-			if (event.data.type === 'result') {
-				report.value = event.data.data;
-				isReporting.value = false;
-			} else if (event.data.type === 'error') {
-				console.error('Worker error:', event.data.data);
-				isReporting.value = false;
-			}
-		};
+  deleteRequest.onblocked = function () {
+    console.warn(`Deletion of database 'CSVDatabase' is blocked. Close all connections.`);
+    indexedDB.close();
+  };
+}
 
-		worker.onerror = function (error) {
-			console.error('Worker error:', error);
-			isReporting.value = false;
-		};
+async function handleFile(fileData: { file: File; data: StandardizedData[]; }) {
+  if (!details.value || !details.value.fileName1) {
+    details.value = {
+      fileName1: fileData.file.name.split('.')[0],
+      fileName2: '',
+    };
+    await storeInIndexedDB(fileData.data, details.value.fileName1);
+  } else if (!details.value.fileName2) {
+    details.value = {
+      fileName1: details.value.fileName1,
+      fileName2: fileData.file.name.split('.')[0],
+    };
+    await storeInIndexedDB(fileData.data, details.value.fileName2);
+  }
+}
 
-		worker.postMessage({
-			type: 'start',
-			map1Data,
-			map2Data,
-		});
-	}
+async function compareFiles() {
+  isReporting.value = true;
+  if (!filesReady.value) {
+    alert('Please select both files');
+    return;
+  }
 
-	async function loadFromIndexedDB(
-		storeName: string
-	): Promise<StandardizedData[]> {
-		return new Promise((resolve, reject) => {
-			const request = indexedDB.open('CSVDatabase', 1);
+  const worker = new Worker(
+    new URL('./compareWorker.ts', import.meta.url),
+    { type: 'module' }
+  );
 
-			request.onupgradeneeded = function (event) {
-				const db = (event.target as IDBOpenDBRequest).result;
-				if (db) {
-					if (!db.objectStoreNames.contains(storeName)) {
-						db.createObjectStore(storeName, {
-							keyPath: 'id',
-							autoIncrement: true,
-						});
-					}
-				}
-			};
+  const map1Data = await loadFromIndexedDB(file1.value!.name.split('.')[0]);
+  const map2Data = await loadFromIndexedDB(file2.value!.name.split('.')[0]);
 
-			request.onsuccess = function (event) {
-				const db = (event.target as IDBOpenDBRequest).result;
-				if (db) {
-					const transaction = db.transaction(storeName, 'readonly');
-					const store = transaction.objectStore(storeName);
-					const data: StandardizedData[] = [];
+  worker.onmessage = function (event) {
+    if (event.data.type === 'result') {
+      report.value = event.data.data;
+      isReporting.value = false;
+    } else if (event.data.type === 'error') {
+      console.error('Worker error:', event.data.data);
+      isReporting.value = false;
+    }
+  };
 
-					store.openCursor().onsuccess = function (e) {
-						const cursor = (
-							e.target as IDBRequest<IDBCursorWithValue>
-						).result;
-						if (cursor) {
-							data.push(cursor.value);
-							cursor.continue();
-						} else {
-							resolve(data);
-						}
-					};
+  worker.onerror = function (error) {
+    console.error('Worker error:', error);
+    isReporting.value = false;
+  };
 
-					transaction.oncomplete = function () {
-						db.close();
-					};
-				} else {
-					reject(new Error('Failed to open IndexedDB'));
-				}
-			};
+  worker.postMessage({
+    type: 'start',
+    map1Data,
+    map2Data,
+  });
+}
 
-			request.onerror = function (event) {
-				const requestError = (event.target as IDBOpenDBRequest).error;
-				console.error('IndexedDB error:', requestError);
-				reject(requestError);
-			};
-		});
-	}
+async function loadFromIndexedDB(storeName: string): Promise<StandardizedData[]> {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open('CSVDatabase', DBversion.value);
 
-	function storeInIndexedDB(
-		data: StandardizedData[],
-		storeName: string
-	) {
-		const request = indexedDB.open('CSVDatabase', 1);
-		console.log('the request is ', request);
-		request.onupgradeneeded = (event) => {
-			const db = (event.target as IDBOpenDBRequest).result;
-			console.log('got DB ', db);
-			console.log('the storenames are ', db.objectStoreNames);
-			if (db) {
-				if (!db.objectStoreNames.contains(storeName)) {
-					console.log('creating db');
-					db.createObjectStore(storeName, {
-						keyPath: 'id',
-						autoIncrement: true,
-					});
-				}
-			}
-		};
+    request.onupgradeneeded = function (event) {
+      const db = (event.target as IDBOpenDBRequest).result;
+      if (db) {
+        if (!db.objectStoreNames.contains(storeName)) {
+          db.createObjectStore(storeName, {
+            keyPath: 'id',
+            autoIncrement: true,
+          });
+        }
+      }
+    };
 
-		request.onsuccess = (event) => {
-			const db = (event.target as IDBOpenDBRequest)
-				.result as IDBDatabase;
-			console.log('the DB is ', db);
-			console.log('the data is ', data);
-			console.log('the storeName is ', storeName);
-			if (db) {
-				const transaction = db.transaction(storeName, 'readwrite');
-				const store = transaction.objectStore(storeName);
+    request.onsuccess = function (event) {
+      const db = (event.target as IDBOpenDBRequest).result;
+      if (db) {
+        const transaction = db.transaction(storeName, 'readonly');
+        const store = transaction.objectStore(storeName);
+        const data: StandardizedData[] = [];
 
-				data.forEach((row) => {
-					store.add(row);
-				});
+        store.openCursor().onsuccess = function (e) {
+          const cursor = (e.target as IDBRequest<IDBCursorWithValue>).result;
+          if (cursor) {
+            data.push(cursor.value);
+            cursor.continue();
+          } else {
+            resolve(data);
+          }
+        };
 
-				transaction.oncomplete = function () {
-					console.log('data stored !');
-					db.close();
-				};
-			} else {
-				console.error('Failed to open IndexedDB');
-			}
-		};
+        transaction.oncomplete = function () {
+          db.close();
+        };
+      } else {
+        reject(new Error('Failed to open IndexedDB'));
+      }
+    };
 
-		request.onerror = function (event) {
-			const requestError = (event.target as IDBRequest<IDBDatabase>)
-				.error;
-			console.error('IndexedDB error:', requestError);
-		};
-	}
+    request.onerror = function (event) {
+      const requestError = (event.target as IDBOpenDBRequest).error;
+      console.error('IndexedDB error:', requestError);
+      reject(requestError);
+    };
+  });
+}
+
+async function storeInIndexedDB(data: StandardizedData[], storeName: string) {
+  const request = indexedDB.open('CSVDatabase', DBversion.value);
+  DBversion.value++;
+
+  request.onupgradeneeded = (event) => {
+    const db = (event.target as IDBOpenDBRequest).result;
+    if (db) {
+      if (!db.objectStoreNames.contains(storeName)) {
+        db.createObjectStore(storeName, {
+          keyPath: 'id',
+          autoIncrement: true,
+        });
+      }
+    }
+  };
+
+  request.onsuccess = (event) => {
+    const db = (event.target as IDBOpenDBRequest).result as IDBDatabase;
+    if (db) {
+      const transaction = db.transaction(storeName, 'readwrite');
+      const store = transaction.objectStore(storeName);
+
+      data.forEach((row) => {
+        store.add(row);
+      });
+
+      transaction.oncomplete = () => {
+        console.log('Data stored successfully');
+        db.close();
+      };
+
+      transaction.onerror = (event) => {
+        console.error('Transaction error:', transaction.error);
+      };
+    } else {
+      console.error('Failed to open IndexedDB');
+    }
+  };
+
+  request.onerror = (event) => {
+    const requestError = (event.target as IDBRequest<IDBDatabase>).error;
+    console.error('IndexedDB error:', requestError);
+  };
+}
+
 </script>
 
 <style>
-	.container {
-		max-width: 600px;
-	}
+.container {
+  max-width: 600px;
+}
 </style>
