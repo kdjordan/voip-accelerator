ba325ed20bbf61d4565eb37ff7e25cf07f50df51
kdjordan <k.dean.jordan@gmail.com>
2024-08-12 19:55:08 -0700

    REFAC: clean up merge conflicts

--- src/components/UploadComponent.vue
+++ src/components/UploadComponent.vue
@@ -1,271 +1,239 @@
 <template>
-	<div class="min-w-[300px] max-w-md rounded overflow-x-auto">
-	<div class="min-w-[300px] max-w-md rounded overflow-x-auto">
-		<div
-			class="px-6 rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4 py-8 bg-background"
-			class="px-6 rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4 py-8 bg-background"
-		>
-			<p class="text-muted-foreground mb-4">{{ displayMessage }}</p>
-			<p class="text-muted-foreground mb-4">{{ displayMessage }}</p>
-			<div
-				:class="[
-					'w-[95%] h-32 border-2 border-primary rounded-md flex items-center justify-center tracking-wide text-primary hover:bg-slate-400/80 transition-colors cursor-pointer',
-					'w-[95%] h-32 border-2 border-primary rounded-md flex items-center justify-center tracking-wide text-primary hover:bg-slate-400/80 transition-colors cursor-pointer',
-					{
-						pulse: DBstore.isComponentFileUploading(
-							props.componentName
-						),
-						fileLoaded: props.disabled,
-					},
-				]"
-				@dragover.prevent="onDragOver"
-				@drop.prevent="onDrop"
-				@dragenter="onDragEnter"
-				@dragleave="onDragLeave"
-				@click="selectFile"
-			>
-				<div class="flex flex-col items-center gap-2">
-					<p
-						:class="{
-							'text-white': DBstore.isComponentFileUploading(
-								props.componentName
-							),
-						}"
-					>
-						{{ statusMessage }}
-					</p>
+  <div class="min-w-[300px] max-w-md rounded overflow-x-auto">
+    <div class="px-6 rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4 py-8 bg-background">
+      <p class="text-muted-foreground mb-4">{{ displayMessage }}</p>
+      <div
+        :class="[
+          'w-[95%] h-32 border-2 border-primary rounded-md flex items-center justify-center tracking-wide text-primary hover:bg-slate-400/80 transition-colors cursor-pointer',
+          {
+            pulse: DBstore.isComponentFileUploading(props.componentName),
+            fileLoaded: props.disabled,
+          },
+        ]"
+        @dragover.prevent="onDragOver"
+        @drop.prevent="onDrop"
+        @dragenter="onDragEnter"
+        @dragleave="onDragLeave"
+        @click="selectFile"
+      >
+        <div class="flex flex-col items-center gap-2">
+          <p
+            :class="{
+              'text-white': DBstore.isComponentFileUploading(
+                props.componentName
+              ),
+            }"
+          >
+            {{ statusMessage }}
+          </p>
 
-					<UploadIcon
-						v-if="!props.disabled"
-						class="w-8 h-8 text-primary"
-					/>
-					
-				</div>
+          <UploadIcon
+            v-if="!props.disabled"
+            class="w-8 h-8 text-primary"
+          />
+        </div>
 
-				<input
-					type="file"
-					@change="handleFileUpload"
-					accept=".csv"
-					hidden
-					:disabled="props.disabled || DBstore.globalFileIsUploading"
-					ref="fileInput"
-				/>
-			</div>
-			<button
-			<button
-				v-if="DBstore.isComponentDisabled(props.componentName)"
-				@click="removeFromDB"
-				class="btn btn-destructive"
-			>
-				Remove
-			</button>
-				class="btn btn-destructive"
-			>
-				Remove
-			</button>
-		</div>
-		<!-- Column Roles Modal -->
-		<TheModal
-			v-show="showModal"
-			:showModal="showModal"
-			:columns="columns"
-			:previewData="previewData"
-			:columnRoles="columnRoles"
-			:startLine="startLine"
-			@confirm="confirmColumnRoles"
-			@cancel="cancelModal"
-			:columnRoleOptions="columnRoleOptions"
-		/>
-	</div>
+        <input
+          type="file"
+          @change="handleFileUpload"
+          accept=".csv"
+          hidden
+          :disabled="props.disabled || DBstore.globalFileIsUploading"
+          ref="fileInput"
+        />
+      </div>
+      <button
+        v-if="DBstore.isComponentDisabled(props.componentName)"
+        @click="removeFromDB"
+        class="btn btn-destructive"
+      >
+        Remove
+      </button>
+    </div>
+    <!-- Column Roles Modal -->
+    <TheModal
+      v-show="showModal"
+      :showModal="showModal"
+      :columns="columns"
+      :previewData="previewData"
+      :columnRoles="columnRoles"
+      :startLine="startLine"
+      @confirm="confirmColumnRoles"
+      @cancel="cancelModal"
+      :columnRoleOptions="columnRoleOptions"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-	import TheModal from './TheModal.vue';
-	import { AZColumnRole } from '../../types/app-types';
-	import UploadIcon from './UploadIcon.vue';
-	import { ref, watch, onMounted } from 'vue';
-	import useCSVProcessing from '../composables/useCsvFilesFunctions';
-	import { useDBstate } from '@/stores/dbStore';
+import TheModal from './TheModal.vue';
+import { AZColumnRole } from '../../types/app-types';
+import UploadIcon from './UploadIcon.vue';
+import { ref, watch } from 'vue';
+import useCSVProcessing from '../composables/useCsvFilesFunctions';
+import { useDBstate } from '@/stores/dbStore';
 
-	//componenet props
-	const props = defineProps<{
-		typeOfComponent: string;
-		DBname: string;
-		componentName: string;
-		disabled: boolean;
-		columnRoleOptions: { value: AZColumnRole; label: string }[];
-	}>();
+// Component props
+const props = defineProps<{
+  typeOfComponent: string;
+  DBname: string;
+  componentName: string;
+  disabled: boolean;
+  columnRoleOptions: { value: AZColumnRole; label: string }[];
+}>();
 
+// Extract functions and reactive properties from useCSVProcessing composable
+const {
+  file,
+  startLine,
+  columnRoles,
+  columns,
+  DBname,
+  showModal,
+  previewData,
+  componentName,
+  parseCSVForFullProcessing,
+  parseCSVForPreview,
+  removeFromDB,
+} = useCSVProcessing();
 
-	// Extract functions and reactive properties from useCSVProcessing composable
-	const {
-		file,
-		startLine,
-		columnRoles,
-		columns,
-		DBname,
-		showModal,
-		previewData,
-		componentName,
-		parseCSVForFullProcessing,
-		parseCSVForPreview,
-		removeFromDB,
-	} = useCSVProcessing();
+// Define reactive properties
+const fileInput = ref<HTMLInputElement | null>(null);
+const isDragOver = ref<boolean>(false);
+const statusMessage = ref<string>('Drag file or click to upload');
+const displayMessage = ref<string>('');
 
-	// Define reactive properties
-	const fileInput = ref<HTMLInputElement | null>(null);
-	const isDragOver = ref<boolean>(false);
-	const statusMessage = ref<string>('Drag file or click to upload');
-	const displayMessage = ref<string>('');
+// Set DB name and component name from props
+const DBstore = useDBstate();
 
-	// Set DB name and component name from props
-	const DBstore = useDBstate();
+DBname.value = props.DBname;
+componentName.value = props.componentName;
 
-	DBname.value = props.DBname;
-	componentName.value = props.componentName;
+// Setup displayMessage based on componentType prop
+const updateDisplayMessage = (type: string) => {
+  if (type === 'owner') {
+    displayMessage.value = 'Upload YOUR rates as CSV';
+  } else if (type === 'client') {
+    displayMessage.value = 'Upload CARRIER rates as CSV';
+  } else if (type === 'complete') {
+    displayMessage.value = DBstore.getStoreNameByComponent(
+      props.componentName
+    );
+  }
+};
 
-	//setup displayMessage based on componentType prop
-	const updateDisplayMessage = (type: string) => {
-		if (type === 'owner') {
-			displayMessage.value = 'Upload YOUR rates as CSV';
-		} else if (type === 'client') {
-			displayMessage.value = 'Upload CARRIER rates as CSV';
-		} else if (type === 'complete') {
-			displayMessage.value = DBstore.getStoreNameByComponent(
-				props.componentName
-			);
-		} else if (type === 'complete') {
-			displayMessage.value = DBstore.getStoreNameByComponent(
-				props.componentName
-			);
-		}
-	};
+// Watch for changes in typeOfComponent prop
+watch(
+  () => props.typeOfComponent,
+  (newType) => {
+    updateDisplayMessage(newType);
+  },
+  { immediate: true }
+);
 
+// Watch for changes in fileUploading and disabled prop
+watch(
+  [
+    () => DBstore.isComponentFileUploading(props.componentName),
+    () => props.disabled,
+  ],
+  ([localDBloadingVal, disabledVal]) => {
+    if (localDBloadingVal) {
+      statusMessage.value = 'Working on it...';
+    } else if (disabledVal) {
+      statusMessage.value = 'Success';
+      updateDisplayMessage('complete');
+    }
+  }
+);
 
-	// Watch for changes in typeOfComponent prop
-	watch(
-		() => props.typeOfComponent,
-		(newType) => {
-			updateDisplayMessage(newType);
-		},
-		{ immediate: true }
-	);
+// Ensure statusMessage and displayMessage are correct if this component has an uploaded file
+if (DBstore.getStoreNameByComponent(props.componentName)) {
+  updateDisplayMessage('complete');
+}
 
+function handleFileUpload(event: Event) {
+  const target = event.target as HTMLInputElement | null;
+  if (target && target.files) {
+    const uploadedFile = target.files[0];
+    if (uploadedFile) {
+      file.value = uploadedFile;
+      parseCSVForPreview(uploadedFile);
+    }
+  }
+}
 
-	// Watch for changes in fileUploading and disabled prop
-	watch(
-		[
-			() => DBstore.isComponentFileUploading(props.componentName),
-			() => props.disabled,
-		],
-		([localDBloadingVal, disabledVal]) => {
-			if (localDBloadingVal) {
-				statusMessage.value = 'Working on it...';
-			} else if (disabledVal) {
-				statusMessage.value = 'Success';
-				updateDisplayMessage('complete');
-				statusMessage.value = 'Success';
-				updateDisplayMessage('complete');
-			}
-		}
-	);
+function resetLocalState() {
+  file.value = null;
+  fileInput.value = null;
+  columns.value = [];
+  previewData.value = [];
+  columnRoles.value = [];
+  statusMessage.value = 'Drag file here or click to load.';
+}
 
-	//if this component has a file that's uploaded make sure stausMessage and displayMessage
-	//are corrrect
-	if (DBstore.getStoreNameByComponent(props.componentName)) {
-		updateDisplayMessage('complete');
-	if (DBstore.getStoreNameByComponent(props.componentName)) {
-		updateDisplayMessage('complete');
-	}
+function onDrop(event: DragEvent) {
+  const uploadedFile = event.dataTransfer?.files[0] ?? null;
+  if (uploadedFile) {
+    file.value = uploadedFile;
+    parseCSVForPreview(uploadedFile);
+  }
+  isDragOver.value = false;
+}
 
-	function handleFileUpload(event: Event) {
-		const target = event.target as HTMLInputElement | null;
-		if (target && target.files) {
-			const uploadedFile = target.files[0];
-			if (uploadedFile) {
-				file.value = uploadedFile;
-				parseCSVForPreview(uploadedFile);
-			}
-		}
-	}
+function onDragOver(event: DragEvent) {
+  if (!DBstore.globalFileIsUploading) {
+    isDragOver.value = false;
+  }
+}
 
-	// onMounted(() => {
-	// 	console.log('mounting', props.disabled)
-	// })
+function onDragEnter(event: DragEvent) {
+  if (!DBstore.globalFileIsUploading) {
+    isDragOver.value = true;
+  }
+}
 
-	function resetLocalState() {
-		file.value = null;
-		fileInput.value = null;
-		columns.value = [];
-		previewData.value = [];
-		columnRoles.value = [];
-		statusMessage.value = 'Drag file here or click to load.';
-	}
+function onDragLeave(event: DragEvent) {
+  if (!DBstore.globalFileIsUploading) {
+    isDragOver.value = false;
+  }
+}
 
-	function onDrop(event: DragEvent) {
-		const uploadedFile = event.dataTransfer?.files[0] ?? null;
-		if (uploadedFile) {
-			file.value = uploadedFile;
-			parseCSVForPreview(uploadedFile);
-		}
-		isDragOver.value = false;
-	}
+interface ColumnRolesEvent {
+  columnRoles: string[];
+  startLine: number;
+}
 
-	function onDragOver(event: DragEvent) {
-		if (!DBstore.globalFileIsUploading) {
-			isDragOver.value = false;
-		}
-	}
+async function confirmColumnRoles(event: ColumnRolesEvent) {
+  showModal.value = false;
+  columnRoles.value = event.columnRoles;
+  startLine.value = event.startLine;
+  await parseCSVForFullProcessing();
+}
 
-	function onDragEnter(event: DragEvent) {
-		if (!DBstore.globalFileIsUploading) {
-			isDragOver.value = true;
-		}
-	}
+function cancelModal() {
+  showModal.value = false;
+}
 
-	function onDragLeave(event: DragEvent) {
-		if (!DBstore.globalFileIsUploading) {
-			isDragOver.value = false;
-		}
-	}
-
-	interface ColumnRolesEvent {
-		columnRoles: string[];
-		startLine: number;
-	}
-
-	async function confirmColumnRoles(event: ColumnRolesEvent) {
-		showModal.value = false;
-		console.log('column roles ', columnRoles);
-		columnRoles.value = event.columnRoles;
-		startLine.value = event.startLine;
-		await parseCSVForFullProcessing();
-	}
-
-	function cancelModal() {
-		showModal.value = false;
-	}
-
-	function selectFile(): void {
-		const input = fileInput.value;
-		if (input) {
-			input.value = '';
-			input.click();
-		}
-	}
+function selectFile(): void {
+  const input = fileInput.value;
+  if (input) {
+    input.value = '';
+    input.click();
+  }
+}
 </script>
 
 <style scoped>
-	.fileLoaded {
-		background-color: hsl(120, 100%, 30%);
-		background-color: hsl(120, 100%, 30%);
-		color: white;
-	}
-	.drop-zone .absolute {
-		transition: width 0.3s ease-in-out;
-	}
-	.drop-zone.no-hover:hover {
-		border-color: inherit; /* Disable hover effect */
-		cursor: not-allowed; /* Optionally change the cursor to indicate disabled state */
-	}
+.fileLoaded {
+  background-color: hsl(120, 100%, 30%);
+  color: white;
+}
+.drop-zone .absolute {
+  transition: width 0.3s ease-in-out;
+}
+.drop-zone.no-hover:hover {
+  border-color: inherit; /* Disable hover effect */
+  cursor: not-allowed; /* Optionally change the cursor to indicate disabled state */
+}
 </style>
--- src/views/AzPricing.vue
+++ src/views/AzPricing.vue
@@ -1,204 +1,129 @@
 <template>
-	<div
-		class="flex flex-col items-center pt-8 h-full"
-	>
-		<div class="flex flex-col items-center w-1/2 mb-16">
-			<h1 class="text-size3xl uppercase mb-2">AZ Pricing</h1>
-			<p class="text-center text-muted-foreground">
-				Simply upload your current rates and the rates of your
-				perspective carrier. We will genrate you a report showing the
-				best opportunities for you to buy and sell.
-			</p>
-		</div>
-		<button
-			@click="resetThisReport"
-			v-if="report"
-			class="btn btn-destructive mb-8"
-			class="btn btn-destructive mb-8"
-		>
-			Reset
-			Reset
-		</button>
-		<div class="h-full flex flex-col items-center">
-		<div class="h-full flex flex-col items-center">
-		<div
-			v-if="!report"
-			class="flex items-center justify-center gap-8 flex-wrap mb-8"
-			class="flex items-center justify-center gap-8 flex-wrap mb-8"
-		>
-			<UploadComponent
-				typeOfComponent="owner"
-				DBname="az"
-				:componentName="component1"
-				:disabled="dbStore.isComponentDisabled('az1')"
-				:columnRoleOptions="columnRoleOptions"
-			/>
+  <div class="flex flex-col items-center pt-8 h-full">
+    <div class="flex flex-col items-center w-1/2 mb-16">
+      <h1 class="text-size3xl uppercase mb-2">AZ Pricing</h1>
+      <p class="text-center text-muted-foreground">
+        Simply upload your current rates and the rates of your prospective carrier. We will generate you a report showing the best opportunities for you to buy and sell.
+      </p>
+    </div>
+    <button
+      @click="resetThisReport"
+      v-if="report"
+      class="btn btn-destructive mb-8"
+    >
+      Reset
+    </button>
+    <div class="h-full flex flex-col items-center">
+      <div
+        v-if="!report"
+        class="flex items-center justify-center gap-8 flex-wrap mb-8"
+      >
+        <UploadComponent
+          typeOfComponent="owner"
+          DBname="az"
+          :componentName="component1"
+          :disabled="dbStore.isComponentDisabled('az1')"
+          :columnRoleOptions="columnRoleOptions"
+        />
 
-			<UploadComponent
-				typeOfComponent="client"
-				DBname="az"
-				:componentName="component2"
-				:disabled="dbStore.isComponentDisabled('az2')"
-				:columnRoleOptions="columnRoleOptions"
-			/>
-		</div>
-		<div>
-			<div
-				v-if="isGeneratingReport"
-				class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg shadow-md cursor-pointer pulse"
-			>
-				<p class="text-center">GERERATING REPORT</p>
-			</div>
-			<button
-				v-if="!isGeneratingReport && !report"
-				@click="makeReport"
-				:disabled="!dbStore.getIsAZfull"
-				:class="{
-					'bg-blue-500 hover:bg-blue-600 text-white':
-						dbStore.getIsAZfull,
-					'bg-gray-500 text-gray-300 cursor-not-allowed':
-						!dbStore.getIsAZfull,
-				}"
-				class="btn"
-				class="btn"
-			>
-				Get Report
-			</button>
-		</div>
-		<div>
-			<GenerateReport v-if="report" :report="report" />
-		</div>
-	</div>
-</div>
+        <UploadComponent
+          typeOfComponent="client"
+          DBname="az"
+          :componentName="component2"
+          :disabled="dbStore.isComponentDisabled('az2')"
+          :columnRoleOptions="columnRoleOptions"
+        />
+      </div>
+      <div>
+        <div
+          v-if="isGeneratingReport"
+          class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg shadow-md cursor-pointer pulse"
+        >
+          <p class="text-center">GENERATING REPORT</p>
+        </div>
+        <button
+          v-if="!isGeneratingReport && !report"
+          @click="makeReport"
+          :disabled="!dbStore.getIsAZfull"
+          :class="{
+            'bg-blue-500 hover:bg-blue-600 text-white': dbStore.getIsAZfull,
+            'bg-gray-500 text-gray-300 cursor-not-allowed': !dbStore.getIsAZfull,
+          }"
+          class="btn"
+        >
+          Get Report
+        </button>
+      </div>
+      <div>
+        <GenerateReport v-if="report" :report="report" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-	import { ref } from 'vue';
-	import { type ComparisonReport, AZColumnRole } from '../../types/app-types';
-	import UploadComponent from '../components/UploadComponent.vue';
-	import GenerateReport from '../components/GenerateReport.vue';
-	import useIndexedDB from '../composables/useIndexDB';
-	import { makePricingReportApi, resetReportApi } from '@/API/api';
-	const { loadFromIndexedDB } = useIndexedDB();
-	import { useDBstate } from '@/stores/dbStore';
+import { ref } from 'vue';
+import { type ComparisonReport, AZColumnRole } from '../../types/app-types';
+import UploadComponent from '../components/UploadComponent.vue';
+import GenerateReport from '../components/GenerateReport.vue';
+import useIndexedDB from '../composables/useIndexDB';
+import { makePricingReportApi, resetReportApi } from '@/API/api';
+import { useDBstate } from '@/stores/dbStore';
 
+const dbStore = useDBstate();
+const { loadFromIndexedDB } = useIndexedDB();
 
-	const dbStore = useDBstate();
+const theDb = ref<string>('az');
+const component1 = ref<string>('az1');
+const component2 = ref<string>('az2');
+const isGeneratingReport = ref<boolean>(false);
+const report = ref<ComparisonReport | null>(null);
 
-	const theDb = ref<string>('az');
-	const component1 = ref<string>('az1');
-	const component2 = ref<string>('az2');
-	// const file1 = ref<string>(dbStore.getStoreNameByComponent(component1.value).split('.')[0])
-	// const file2 = ref<string>(dbStore.getStoreNameByComponent(component2.value).split('.')[0])
-	// const fileName1 = ref<string>('');
-	// const fileName2 = ref<string>('');
-	const isGeneratingReport = ref<boolean>(false);
-	const report = ref<ComparisonReport | null>(null);
-
-	const columnRoleOptions = [
+const columnRoleOptions = [
   { value: AZColumnRole.Destination, label: 'Destination Name' },
   { value: AZColumnRole.DialCode, label: 'Dial Code' },
   { value: AZColumnRole.Rate, label: 'Rate' },
 ];
 
-	async function resetThisReport() {
-		//use API to delet DB by name
-		await resetReportApi('az');
-		//reset Pinia state
-		dbStore.resetFilesUploadedByDBname('az');
-		dbStore.resetFilesUploadedByDBname('az');
-		report.value = null;
-	}
+async function resetThisReport() {
+  await resetReportApi('az');
+  dbStore.resetFilesUploadedByDBname('az');
+  report.value = null;
+}
 
-	async function makeReport() {
-		isGeneratingReport.value = true;
-		let fileName1 = dbStore
-			.getStoreNameByComponent(component1.value)
-			.split('.')[0];
-		let fileName2 = dbStore
-			.getStoreNameByComponent(component2.value)
-			.split('.')[0];
+async function makeReport() {
+  isGeneratingReport.value = true;
 
-		let file1Data = await getFilesFromIndexDB(
-			theDb.value,
-			dbStore.getStoreNameByComponent(component1.value),
-			dbStore.globalDBVersion
-		);
-		let file2Data = await getFilesFromIndexDB(
-			theDb.value,
-			dbStore.getStoreNameByComponent(component2.value),
-			dbStore.globalDBVersion
-		);
+  const fileName1 = dbStore.getStoreNameByComponent(component1.value).split('.')[0];
+  const fileName2 = dbStore.getStoreNameByComponent(component2.value).split('.')[0];
 
-		let fileName1 = dbStore
-			.getStoreNameByComponent(component1.value)
-			.split('.')[0];
-		let fileName2 = dbStore
-			.getStoreNameByComponent(component2.value)
-			.split('.')[0];
+  const file1Data = await getFilesFromIndexDB(theDb.value, dbStore.getStoreNameByComponent(component1.value), dbStore.globalDBVersion);
+  const file2Data = await getFilesFromIndexDB(theDb.value, dbStore.getStoreNameByComponent(component2.value), dbStore.globalDBVersion);
 
-		let file1Data = await getFilesFromIndexDB(
-			theDb.value,
-			dbStore.getStoreNameByComponent(component1.value),
-			dbStore.globalDBVersion
-		);
-		let file2Data = await getFilesFromIndexDB(
-			theDb.value,
-			dbStore.getStoreNameByComponent(component2.value),
-			dbStore.globalDBVersion
-		);
+  if (fileName1 && fileName2 && file1Data && file2Data) {
+    console.log('starting the report...');
+    const returnedReport = await makePricingReportApi({
+      fileName1,
+      fileName2,
+      file1Data,
+      file2Data,
+    });
+    console.log('got report ', returnedReport);
+    report.value = returnedReport;
+    isGeneratingReport.value = false;
+  } else {
+    isGeneratingReport.value = false;
+    console.error('Error getting files from DB');
+  }
+}
 
-		if (fileName1 && fileName2 && file1Data && file2Data) {
-			console.log('starting the report...');
-			const returnedReport = await makePricingReportApi({
-				fileName1,
-				fileName2,
-				file1Data,
-				file2Data,
-			});
-			console.log('got report ', returnedReport);
-			report.value = returnedReport;
-				file2Data,
-			});
-			console.log('got report ', returnedReport);
-			report.value = returnedReport;
-			isGeneratingReport.value = false;
-		} else {
-			isGeneratingReport.value = false;
-			console.error('Error getting files from DB');
-			console.error('Error getting files from DB');
-		}
-	}
-
-	async function getFilesFromIndexDB(
-		dbName: string,
-		store: string,
-		dbVersion: number
-	) {
-
-	async function getFilesFromIndexDB(
-		dbName: string,
-		store: string,
-		dbVersion: number
-	) {
-		try {
-			const result = await loadFromIndexedDB(
-				dbName,
-				store,
-				dbVersion
-			);
-			return result;
-		} catch (e) {
-			const result = await loadFromIndexedDB(
-				dbName,
-				store,
-				dbVersion
-			);
-			return result;
-		} catch (e) {
-			isGeneratingReport.value = false;
-			console.error(`got an errror getting ${store} out of DB`);
-			console.error(`got an errror getting ${store} out of DB`);
-		}
-	}
+async function getFilesFromIndexDB(dbName: string, store: string, dbVersion: number) {
+  try {
+    const result = await loadFromIndexedDB(dbName, store, dbVersion);
+    return result;
+  } catch (e) {
+    isGeneratingReport.value = false;
+    console.error(`got an error getting ${store} out of DB`);
+  }
+}
 </script>
