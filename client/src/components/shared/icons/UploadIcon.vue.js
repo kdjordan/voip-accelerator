58ef113f43b7721171438116398345ac6d2b4a83
kdjordan <k.dean.jordan@gmail.com>
2025-02-23 14:33:54 -0800

    CHORE: removed cached js files

---
+++ client/src/components/shared/icons/UploadIcon.vue.js
@@ -0,0 +1,54 @@
+const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, } = await import('vue');
+const __VLS_props = defineProps();
+; /* PartiallyEnd: #3632/scriptSetup.vue */
+const __VLS_fnComponent = (await import('vue')).defineComponent({});
+;
+let __VLS_functionalComponentProps;
+function __VLS_template() {
+    const __VLS_ctx = {};
+    const __VLS_localComponents = {
+        ...{},
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_components;
+    const __VLS_localDirectives = {
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_directives;
+    let __VLS_styleScopedClasses;
+    // CSS variable injection 
+    // CSS variable injection end 
+    let __VLS_resolvedLocalAndGlobalComponents;
+    __VLS_elementAsFunction(__VLS_intrinsicElements.svg, __VLS_intrinsicElements.svg)({ xmlns: ("http://www.w3.org/2000/svg"), width: ("24"), height: ("24"), viewBox: ("0 0 24 24"), fill: ("none"), stroke: ("currentColor"), "stroke-width": ("2"), "stroke-linecap": ("round"), "stroke-linejoin": ("round"), ...(__VLS_ctx.$attrs), });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.path)({ d: ("M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"), });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.polyline)({ points: ("17 8 12 3 7 8"), });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.line)({ x1: ("12"), x2: ("12"), y1: ("3"), y2: ("15"), });
+    var __VLS_slots;
+    var __VLS_inheritedAttrs;
+    const __VLS_refs = {};
+    var $refs;
+    var $el;
+    return {
+        attrs: {},
+        slots: __VLS_slots,
+        refs: $refs,
+        rootEl: $el,
+    };
+}
+;
+const __VLS_self = (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+    __typeProps: {},
+});
+export default (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+    __typeProps: {},
+    __typeEl: {},
+});
+; /* PartiallyEnd: #4569/main.vue */
