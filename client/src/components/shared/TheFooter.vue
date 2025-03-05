c3977a946ae827595d43436339a380c6a60df09f
kdjordan <k.dean.jordan@gmail.com>
2025-03-04 19:26:13 -0800

    REFACTOR: glow up typography

--- client/src/components/shared/TheFooter.vue
+++ client/src/components/shared/TheFooter.vue
@@ -1,8 +1,14 @@
 <template>
   <footer class="text-muted-foreground py-3 text-sizeSm text-foreground">
-    <div class="flex items-center justify-center">
-      <div class="flex items-center justify-start space-x-4">
-        <RouterLink to="#" class="hover:text-foreground/50 transition-colors">
+    <div class="">
+      <div class="mt-12 pl-8 text-fbWhite/50 text-sm">
+        &copy; {{ new Date().getFullYear() }} VOIP Accelerator. All rights reserved.
+      </div>
+      <!-- <div class="flex items-center justify-start space-x-4">
+        <RouterLink
+          to="#"
+          class="hover:text-foreground/50 transition-colors"
+        >
           Pricing
         </RouterLink>
         <span class="text-foreground/50">|</span>
@@ -14,30 +20,35 @@
           FAQ
         </RouterLink>
         <span class="text-foreground/50">|</span>
-        <RouterLink to="#" class="hover:text-foreground/50 transition-colors">
+        <RouterLink
+          to="#"
+          class="hover:text-foreground/50 transition-colors"
+        >
           Legal
         </RouterLink>
         <span class="text-foreground/50">|</span>
-        <RouterLink to="#" class="hover:text-foreground/50 transition-colors">
+        <RouterLink
+          to="#"
+          class="hover:text-foreground/50 transition-colors"
+        >
           Privacy
         </RouterLink>
         <span class="text-foreground/50">|</span>
-        <a 
-          href="#" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
           class="hover:text-foreground/50 transition-colors flex items-center"
         >
           FlatBrim
           <ArrowTopRightOnSquareIcon class="w-3 h-3 ml-1" />
         </a>
-      </div>
+      </div> -->
     </div>
   </footer>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
-import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline';
-
+  import { RouterLink } from 'vue-router';
+  import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline';
 </script>
