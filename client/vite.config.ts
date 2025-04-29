a54d6ca607ee16312fe47971f66f9ea5490961e3
kdjordan <k.dean.jordan@gmail.com>
2025-04-28 17:54:05 -0700

    deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,7 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
